00a18d81ad530b0a631d812b8370b252f3715d3d
Dbachelor <84332279+Dbachelor@users.noreply.github.com>
2023-08-11 17:44:53 +0100

    completed PostController class

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -15,22 +15,27 @@ class PostController extends AbstractController
     public function __construct(PostService $postService){
         $this->postService = $postService;
     }
-    
+
     #[Route('/posts', name: 'app_post', methods:['GET'])]
     public function index(): JsonResponse
     {
         $posts = $this->postService->getPosts(isset($_GET['page'])? $_GET['page']:1, isset($_GET['search'])? $_GET['search']:null);
+        //check for errors
+
         if (!is_array($posts)){
-            return $this->json(["succes"=>false, "message"=>"something went wrong"], 500);
+            return $this->json(['success'=>false, 'message'=>'something went wrong'], 500);
         }
-        if(count($posts) == 0){
-            $message = "no post found";
-            if(isset($_GET['search'])){
-                $message = "no post found for keyword " . $_GET['search'];
+
+        // check if no record is returned
+        if (count($posts) == 0){
+            $message = 'no record found';
+            if (isset($_GET['search'])){
+                $message = 'no record found for ' . $_GET['search'];
             }
-            return $this->json(["succes"=>true, "message"=>$message], 404);
+            return $this->json(['success'=>false, 'message'=>$message], 404);
         }else{
-            return $this->json(["succes"=>true, "data"=>$posts], 200);
+            return $this->json(['success'=>true, 'data'=>$posts], 200);
         }
+       
     }
 }
